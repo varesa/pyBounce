d6fb244c38db9811e9938c2cfd1bf015f55becea
Esa Varemo <esa@kuivanto.fi>
2012-07-24 23:53:18 +0300

    Ball extends pygame Sprite, and loads its image

--- Ball.py
+++ Ball.py
@@ -1,3 +1,9 @@
-class Ball:
+import pygame
+
+ballSprite = "ball.gif"
+
+class Ball(pygame.sprite.Sprite):
     def __init__(self):
-        pass
+        pygame.sprite.Sprite.__init__(self) # Sprite init
+        self.image = pygame.image.load(ballSprite).convert()
+        self.rect = self.image.get_rect()
