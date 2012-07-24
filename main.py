2b85ed66e38aa15db12ee44bab133c65201c7993
Esa Varemo <esa@kuivanto.fi>
2012-07-24 23:20:29 +0300

    Added the main pygame structure

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+import time
+
+pygame.init()
+screen = pygame.display.set_mode([800, 600])
+
+running = True
+
+while running:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                        running = False
+
