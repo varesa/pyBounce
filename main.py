31e07e70dfd2630d95108e174f827ccec8df72a8
Esa Varemo <esa@kuivanto.fi>
2012-07-24 23:31:48 +0300

    Create a wanted amount of balls at startup

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import pygame
 import sys
 import time
 
+from Ball import Ball
+
+numberOfBalls = 1
+
 pygame.init()
 screen = pygame.display.set_mode([800, 600])
+
+balls = []
+
+for x in range(numberOfBalls):
+        balls.append(Ball())
 
 running = True
 
