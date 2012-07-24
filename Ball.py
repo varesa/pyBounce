e0368b5641f43b459852172c9343922199a3da3a
Esa Varemo <esa@kuivanto.fi>
2012-07-25 00:16:39 +0300

    Create and draw balls in random places

--- Ball.py
+++ Ball.py
@@ -1,9 +1,14 @@
 import pygame
+import random
 
 ballSprite = "ball.gif"
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, scrSize):
         pygame.sprite.Sprite.__init__(self) # Sprite init
         self.image = pygame.image.load(ballSprite).convert()
         self.rect = self.image.get_rect()
+
+        self.rect.x = random.randrange(0, scrSize[0] - self.rect.width)
+        self.rect.y = random.randrange(0, scrSize[1] - self.rect.height)
+        
--- main.py
+++ main.py
@@ -8,11 +8,14 @@ numberOfBalls = 1
 
 pygame.init()
 screen = pygame.display.set_mode([800, 600])
+clock = pygame.time.Clock()
 
 balls = []
 
 for x in range(numberOfBalls):
-        balls.append(Ball())
+        balls.append(Ball(screen.get_size()))
+
+allsprites = pygame.sprite.RenderPlain(balls)
 
 background = pygame.Surface(screen.get_size()).convert()
 background.fill((250, 250, 250))
@@ -23,6 +26,8 @@ while running:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                         running = False
+        allsprites.update()
         screen.blit(background, (0, 0))
+        allsprites.draw(screen)
         pygame.display.flip()
 
