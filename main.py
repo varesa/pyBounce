06500ea0cc872638f804838848020ee1a9f6ba4c
Esa Varemo <esa@kuivanto.fi>
2012-07-24 23:52:10 +0300

    Added a background to be rendered

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ balls = []
 for x in range(numberOfBalls):
         balls.append(Ball())
 
+background = pygame.Surface(screen.get_size()).convert()
+background.fill((250, 250, 250))
+
 running = True
 
 while running:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                         running = False
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
 
