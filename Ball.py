884630e6464cc40ec4983fec65ddcaeade615183
Esa Varemo <esa@kuivanto.fi>
2012-07-25 00:48:11 +0300

    Ball position updating and edge detection

--- Ball.py
+++ Ball.py
@@ -13,5 +13,17 @@ class Ball(pygame.sprite.Sprite):
         self.rect.x = random.randrange(0, self.game.screen.get_size()[0] - self.rect.width)
         self.rect.y = random.randrange(0, self.game.screen.get_size()[1] - self.rect.height)
 
-        
+        self.xvel = random.randrange(-10, 10)
+        self.yvel = random.randrange(-10, 10)
+
+    def update(self):
+        self.detectCollisions()
+        self.rect.x += self.xvel
+        self.rect.y += self.yvel
+
+    def detectCollisions(self):
+        if self.rect.x <= 0 or self.rect.x >= self.game.screen.get_size()[0] - self.rect.width:
+            self.xvel *= -1
+        if self.rect.y <= 0 or self.rect.y >= self.game.screen.get_size()[1] - self.rect.height:
+            self.yvel *= -1
         
